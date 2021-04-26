c969f9f4e03a57808d839e9fc73c6ebff76e40f3
Ewa Iwan <38181460+Ewwan@users.noreply.github.com>
2021-04-26 16:27:21 +0200

    fix: webpack scss config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
 const rules = [
   {
     test: /\.tsx?/,
     exclude: /node_modules/,
     loader: "babel-loader"
+  },
+  {
+    test: /\.module\.s(a|c)ss$/,
+    loader: [
+      isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: true,
+          sourceMap: isDevelopment
+        }
+      },
+      {
+        loader: "sass-loader",
+        options: {
+          sourceMap: isDevelopment
+        }
+      }
+    ]
+  },
+  {
+    test: /\.s(a|c)ss$/,
+    exclude: /\.module.(s(a|c)ss)$/,
+    loader: [
+      isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+      "css-loader",
+      {
+        loader: "sass-loader",
+        options: {
+          sourceMap: isDevelopment
+        }
+      }
+    ]
   }
 ];
 
@@ -16,9 +53,15 @@ module.exports = {
     filename: "bundle.js"
   },
   module: { rules },
-  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"] },
   devServer: {
     contentBase: "./",
     port: 5000
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
+  ]
 };
